9aaa1b9eb1ee72daf217748dd1fdadc58213d774
Hardik Parmar <hardikpersonal11@gmail.com>
2025-08-28 09:41:29 +0530

    Adding .env and index.json files

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000", // Replace with your frontend Render URL
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth',userRoutes);
@@ -25,7 +28,7 @@ mongoose.connect(process.env.MONGO_URL,{
 });
 
 
-const server = app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT || 5000,()=>{
     console.log(`server started on PORT ${process.env.PORT}`)
 });
 
